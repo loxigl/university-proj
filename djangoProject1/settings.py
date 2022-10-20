223bdf331641b46b15004539179321d3c8ce7a3a
loxigl <43906792+loxigl@users.noreply.github.com>
2022-10-20 12:37:46 +0300

    start

--- djangoProject1/settings.py
+++ djangoProject1/settings.py
@@ -80,11 +80,11 @@ WSGI_APPLICATION = 'djangoProject1.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'HOST': os.environ.get('POSTGRES_HOST', 'localhost'),
-        'NAME': os.environ.get('POSTGRES_DB', 'db_name'),
-        'USER': os.environ.get('POSTGRES_USER', 'username'),
-        'PASSWORD': os.environ.get('POSTGRES_PASSWORD', 'password'),
-        'PORT': os.environ.get('POSTGRES_PORT', '5432'),
+        'HOST': os.environ.get('POSTGRES_HOST'),
+        'NAME': os.environ.get('POSTGRES_DB', 'db.sqlite3'),
+        'USER': os.environ.get('POSTGRES_USER', ),
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD', ),
+        'PORT': os.environ.get('POSTGRES_PORT', ),
     }
 }
 
@@ -121,6 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR / 'static')
 # MEDIA_URL = 'media/'
 # MEDIA_ROOT = os.path.join(BASE_DIR / 'media')
 
